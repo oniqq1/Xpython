e7ffbd9cb8bb2ab54533aba6fe04a792ac9193ef
ONIQQ <insolencem1.1@gmail.com>
2024-08-09 11:35:14 +0300

    v1.1 нпочти нечего не работает

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import Scripts
 
 # all functions will be in file "Scripts.py" , start script "main.py"
+# не все сделано , пожалуйста не бейте в коментариях
 
 if __name__ == '__main__':
     Scripts.start_window()
