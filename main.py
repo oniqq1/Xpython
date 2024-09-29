1e50b8af40b2219e6c0ea935d2fc1c0b54083dfa
ONIQQ <insolencem1.1@gmail.com>
2024-09-29 16:48:07 +0300

    Create empty git

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-import Scripts
 
-# all functions will be in file "Scripts.py" , start script "main.py"
-# не все сделано , пожалуйста не бейте в коментариях
-
-if __name__ == '__main__':
-    Scripts.start_window()
--- test_Scripts.py
+++
@@ -1,6 +0,0 @@
-from unittest import TestCase
-import Scripts
-
-class TestScripts(TestCase):
-    def test_group_1(self):
-        self.assertRaises()
